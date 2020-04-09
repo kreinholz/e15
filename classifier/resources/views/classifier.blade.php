a35105d0bed7a9d0fc8599c2b8abdace08809dad
Kevin Reinholz <kreinholz@gmail.com>
2020-04-09 10:58:46 -0700

    Modified classifier to use POST method for submitting text to categorize via machine learning. Updated classifier home page image with a proper textarea instead of a text input.

--- classifier/resources/views/classifier.blade.php
+++ classifier/resources/views/classifier.blade.php
@@ -18,9 +18,12 @@
             Copy and paste the text of a news article to have AI classify it into a category.
         </p>
         
-        <form method='GET' action='/classify'>
+        <form method='POST' action='/classify'>
+
+            {{ csrf_field() }}
+
             <label for='inputString'>Article text:</label>
-            <input type='textarea' rows='20' cols='50' id='articleText' name='articleText' value='{{ old('articleText', $articleText) }}'> 
+            <textarea name='articleText'>{{ old('articleText', $articleText) }}</textarea>
             <p></p>    
             @if($errors->get('articleText'))
                 <div class='error'>This field must be filled out with plain text (no images) from a news article.</div>
@@ -31,7 +34,7 @@
         <p></p>
         @if($predicted)
         <h2>Machine Learning Classification Results</h2>
-        <p>The computer classified your article as {{ $predicted }}</p>
+        <p>The computer classified your article as {{ $predicted }}.</p>
         <p></p>
         @endif
 
--- classifier/routes/web.php
+++ classifier/routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'ClassifierController@index');
-Route::get('/classify', 'ClassifierController@classify');
+Route::post('/classify', 'ClassifierController@classify');
